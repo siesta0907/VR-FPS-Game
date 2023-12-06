103be45c01ada894f21fcfc53cf94d3af57ea6ba
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2023-12-06 14:58:14 +0900

    고양이 AI 작업

---
+++ Assets/WandManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandManager : MonoBehaviour
+{
+    public SteamVR_TrackObject mTrackObj;
+   // private SteamVR_Controller.Device mwand { get }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
